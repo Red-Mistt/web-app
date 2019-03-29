f3738ff92cba3f157ee6f834ff89bfe63041ea65
Reece Morgan <r.morgan96@sky.com>
2019-03-29 22:35:15 +0000

    Started adding in form for booking tickets

--- php/book-tickets.php
+++ php/book-tickets.php
@@ -4,8 +4,17 @@
 ?>
 
 <div class="main-container center">
-    <p>To be able to book tickets, please sign in first.</p>
-    <p><a href="login.php">Click here</a> to sign in.</p>
+    <!-- If user is logged out -->
+    <!-- <p>To be able to book tickets, please sign in first.</p>
+    <p><a href="login.php">Click here</a> to sign in.</p> -->
+
+    <!-- If uer is logged in -->
+    <div class="container">
+        <label for="username"><b>Username:</b></label>
+        <input type="text" placeholder="Enter Username" name="username" required>
+
+        <button type="submit">submit</button>
+    </div>
 </div>
 
 <?php
