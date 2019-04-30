e09dba8881f11031074b3271d85272c5fcbf4a72
Reece Morgan <r.morgan96@sky.com>
2019-04-30 23:38:42 +0100

    Added logo to header.php files

--- php/shared/header.php
+++ php/shared/header.php
@@ -31,7 +31,7 @@ session_start();
         <header class="navbar">
             <div class="header">
                 <div class="column left">
-                    <a href="../index.php"><img src="../assets/favicon.png" class="logo"/></a>
+                    <a href="/index.php"><img src="../assets/images/SportsCampLogog.png" class="logo"/></a>
                 </div>
                 <div class="column middle">
                     <h1>Sports Camp</h1>
